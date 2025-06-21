a3b2cd22fc81b8777ce7a467c3d96d170cb5f92d
227Faddi <falukhouma27@gmail.com>
2025-06-21 15:25:07 -0400

    setup: add unused variable eslint

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,8 +1,22 @@
 module.exports = {
   extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "unused-imports"],
   rules: {
     "prettier/prettier": "warn",
+    "no-unused-vars": [
+      "warn",
+      { vars: "all", args: "after-used", ignoreRestSiblings: true },
+    ],
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
   },
   settings: {
     "import/resolver": {
