eaa33997bb41ac8e9c8957d3a17bbcd82a14f0d2
227Faddi <falukhouma27@gmail.com>
2025-07-02 01:42:24 -0400

    feat: add mmkv and zustand for state managment and persistance

---
+++ mobile/src/store/storage.ts
@@ -0,0 +1,21 @@
+import { MMKV } from "react-native-mmkv";
+import { StateStorage } from "zustand/middleware";
+
+export const storage = new MMKV({
+  id: "app-storage",
+  encryptionKey: "secure-key",
+});
+
+export const zustandStorage: StateStorage = {
+  setItem: (name: string, value: string) => {
+    storage.set(name, value);
+  },
+  getItem: (name: string) => {
+    const value = storage.getString(name);
+    return value ?? null;
+  },
+  removeItem: (name: string) => {
+    storage.delete(name);
+  },
+};
+export default zustandStorage;
